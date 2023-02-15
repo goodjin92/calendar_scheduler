5d5c69bcf5a88bf2bd7cf5fb31ad3ee896de95f0
Gukjin Kim <exodus0315@outlook.com>
2023-02-15 23:14:19 +0900

    Add calendarScreen

---
+++ lib/component/calendar.dart
@@ -0,0 +1,76 @@
+import 'package:calendar_scheduler/const/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class Calendar extends StatefulWidget {
+  const Calendar({Key? key}) : super(key: key);
+
+  @override
+  State<Calendar> createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  DateTime? selectedDay;
+
+  @override
+  Widget build(BuildContext context) {
+    final defaultBoxDeco = BoxDecoration(
+      color: Colors.grey[200],
+      borderRadius: BorderRadius.circular(6.0),
+    );
+
+    final defaultTextStyle = TextStyle(
+      color: Colors.grey[600],
+      fontWeight: FontWeight.w700,
+    );
+
+    return TableCalendar(
+      focusedDay: DateTime.now(),
+      firstDay: DateTime(1800),
+      lastDay: DateTime(3000),
+      headerStyle: HeaderStyle(
+        formatButtonVisible: false,
+        titleCentered: true,
+        titleTextStyle: TextStyle(
+          fontWeight: FontWeight.w700,
+          fontSize: 16.0,
+        ),
+      ),
+      calendarStyle: CalendarStyle(
+        isTodayHighlighted: false,
+        defaultDecoration: defaultBoxDeco,
+        // 평일만
+        weekendDecoration: defaultBoxDeco,
+        // 주말만
+        selectedDecoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(6.0),
+          border: Border.all(
+            color: PRIMARY_COLOR,
+            width: 1.0,
+          ),
+        ),
+        defaultTextStyle: defaultTextStyle,
+        weekendTextStyle: defaultTextStyle,
+        selectedTextStyle: defaultTextStyle.copyWith(
+          color: PRIMARY_COLOR,
+        ),
+      ),
+      onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
+        print(selectedDay);
+        setState(() {
+          this.selectedDay = selectedDay;
+        });
+      },
+      selectedDayPredicate: (DateTime date) {
+        // date = selectedDay; <- 이걸 안하는 이유는 시, 분, 초까지 같을 필요가 없기 때
+        if (selectedDay == null) {
+          return false;
+        }
+        return date.year == selectedDay!.year &&
+            date.month == selectedDay!.month &&
+            date.day == selectedDay!.day;
+      },
+    );
+  }
+}
